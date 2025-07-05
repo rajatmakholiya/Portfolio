faae94ccfb1d377e7f806d4167d933dbb9cef936
venom96 <rajatmakholiya07@gmail.com>
2025-07-05 12:46:51 +0530

    home page UI Changes

--- src/components/AbstractBio.jsx
+++ src/components/AbstractBio.jsx
@@ -1,14 +1,23 @@
-import { Stack, Typography } from "@mui/material";
-import React from "react";
+import { Stack, Typography, Button } from "@mui/material";
 import Image from "../assets/Profile.jpg";
-import Grid from '@mui/material/Grid';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-export default function AbstractBio() {
+import Grid from "@mui/material/Grid";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import DownloadIcon from "@mui/icons-material/Download";
 
+export default function AbstractBio() {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href =
+      "https://drive.google.com/uc?export=download&id=1fPuyjRIJd88s5_ekKE3c4VMAhLSheTJa";
+    link.download = "Rajat.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
-    <Grid container spacing={2}  alignItems={"center"}>
+    <Grid container spacing={2} alignItems={"center"}>
       <Grid size={2}></Grid>
       <Grid size={2}>
         <Stack
@@ -17,33 +26,91 @@ export default function AbstractBio() {
           alignItems={"center"}
           spacing={1}
         >
-          <img src={Image} alt="Profile Pic" className="max-w-36 max-h-[80vh]" />
+          <img
+            src={Image}
+            alt="Profile Pic"
+            className="max-w-36 max-h-[80vh]"
+          />
           <Typography variant="h5" fontWeight={500}>
             Rajat Makholiya
           </Typography>
           <Typography variant="subtitle1" fontWeight={500}>
             Web Developer
           </Typography>
-          <Stack direction={"row"} spacing={2} alignItems={"center"}
-          >
-            <EmailOutlinedIcon onClick={() => window.open("mailto:rajatmakholiya07@gmail.com")} cursor="pointer" />
-            <LinkedInIcon onClick={() => window.open("https://www.linkedin.com/in/rajat-makholiya-1b92a1222/")} cursor="pointer"/>
-            <GitHubIcon onClick={() => window.open("https://github.com/rajatmakholiya")} cursor="pointer"/>
+          <Stack direction={"row"} spacing={2} alignItems={"center"}>
+            <EmailOutlinedIcon
+              onClick={() => window.open("mailto:rajatmakholiya07@gmail.com")}
+              cursor="pointer"
+            />
+            <LinkedInIcon
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/rajat-makholiya-1b92a1222/"
+                )
+              }
+              cursor="pointer"
+            />
+            <GitHubIcon
+              onClick={() => window.open("https://github.com/rajatmakholiya")}
+              cursor="pointer"
+            />
           </Stack>
+
+          <Button
+            startIcon={<DownloadIcon />}
+            variant="contained"
+            onClick={handleDownload}
+            sx={{
+              borderRadius: "10px",
+              background: "rgba(255, 255, 255, 0.1)",
+              backdropFilter: "blur(8px)",
+              WebkitBackdropFilter: "blur(8px)",
+              color:"whitesmoke",
+              border: "1px solid rgba(255, 255, 255, 0.3)",
+              transition: "all 0.3s ease",
+              "&:hover": {
+                background: "rgba(255, 255, 255, 0.2)",
+                borderColor: "rgba(255, 255, 255, 0.5)",
+              },
+            }}
+          >
+            <Typography
+              variant="subtitle2"
+              textTransform={"none"}
+              noWrap
+              fontWeight={600}
+            >
+              Download CV
+            </Typography>
+          </Button>
         </Stack>
       </Grid>
       <Grid size={7}>
         <Stack textAlign={"center"}>
           <Typography variant="h1" fontWeight={700} fontFamily={"Poppins"}>
-           Hi! I am <Typography variant="h1" component={"span"} fontWeight={700} fontFamily={"cursive"} color="#d9d9c0">Rajat</Typography>
+            Hi! I am{" "}
+            <Typography
+              variant="h1"
+              component={"span"}
+              fontWeight={700}
+              fontFamily={"cursive"}
+              color="#d9d9c0"
+            >
+              Rajat
+            </Typography>
           </Typography>
           <Typography variant="h2" fontWeight={700} fontFamily={"Poppins"}>
-           A Developer And Software Engineer
+            A Developer And Software Engineer
           </Typography>
-         <Typography variant="subtitle2" fontWeight={500} fontFamily={"Poppins"} marginTop={3}>
-          I'm passionate about crafting experiences and exploring new horizons.
-         </Typography>
-        
+          <Typography
+            variant="subtitle2"
+            fontWeight={500}
+            fontFamily={"Poppins"}
+            marginTop={3}
+          >
+            I'm passionate about crafting experiences and exploring new
+            horizons.
+          </Typography>
         </Stack>
       </Grid>
       <Grid size={1}></Grid>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,25 @@
 import React from "react";
 import Abstract from "../components/Abstract";
-import { Button, Stack, Typography } from "@mui/material";
-import Grid from "@mui/material/Grid";  
+import { Stack, Typography } from "@mui/material";
 
 import SchoolIcon from "@mui/icons-material/School";
-import DownloadIcon from "@mui/icons-material/Download";
+import { Opacity } from "@mui/icons-material";
 
 const Home = () => {
-  const handleDownload = () => {
-    const link = document.createElement("a");
-    link.href =
-      "https://drive.google.com/uc?export=download&id=1fPuyjRIJd88s5_ekKE3c4VMAhLSheTJa";
-    link.download = "HiredResume.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Abstract />
       <div className="mx-5 my-10 px-20 ">
         <Stack>
-          <Typography variant="h5" fontWeight={700} paddingY={2} textAlign={"center"}>
+          <Typography
+            variant="h5"
+            fontWeight={700}
+            paddingY={2}
+            textAlign={"center"}
+          >
             A Little About Me
           </Typography>
-          <Typography variant="subtitle2" textAlign={"justify"}>
+          <Typography variant="subtitle1" textAlign={"justify"}>
             I’m a <b>Full Stack Developer</b> focused on building clean,
             scalable, and impactful web applications. With over two years of
             hands-on experience, I specialize in creating responsive and
@@ -41,85 +36,135 @@ const Home = () => {
             turning complex challenges into intuitive digital experiences. Take
             a look at my work and let’s build something impactful together!
           </Typography>
-          <Button
-            startIcon={<DownloadIcon />}
-            variant="contained"
-            onClick={handleDownload}
-            sx={{
-              borderRadius: "7px",
-              width: "180px",
-              height: "40px",
-              marginY: "20px",
-              bgcolor: "#1a1a1a",
-              color: "white",
-              border: "1px solid rgba(255, 255, 255, 0.2)",
-              "&:hover": {
-                backgroundColor: "#222",
-              },
-            }}
+          <br />
+          {/* <hr /> */}
+          <Stack
+            container
+            direction={"row"}
+            justifyContent={"space-between"}
+            marginTop={3}
           >
-            <Typography
-              variant="subtitle2"
-              textTransform={"none"}
-              noWrap
-              fontWeight={500}
+            <Stack spacing={1}>
+              <Typography variant="h6" fontWeight={700}>
+                Pationate About
+              </Typography>
+              <Stack spacing={1}>
+                <Typography
+                  variant="body1"
+                  paddingLeft={2}
+                  color="textDisabled"
+                  fontWeight={500}
+                >
+                  Artificial Intelligence
+                </Typography>
+                <Typography
+                  variant="body1"
+                  paddingLeft={2}
+                  color="textDisabled"
+                  fontWeight={500}
+                >
+                  Blockchain and DeFi
+                </Typography>
+                <Typography
+                  variant="body1"
+                  paddingLeft={2}
+                  color="textDisabled"
+                  fontWeight={500}
+                >
+                  DevOps and Cloud
+                </Typography>
+              </Stack>
+            </Stack>
+            <Stack spacing={2}>
+              <Typography variant="h6" fontWeight={700}>
+                Education
+              </Typography>
+              <Stack direction={"row"} spacing={2} alignItems={"center"}>
+                <SchoolIcon />
+                <Stack>
+                  <Typography
+                    variant="body1"
+                    color="textDisabled"
+                    fontWeight={500}
+                  >
+                    Bachelor of Computer Application
+                  </Typography>
+                  <Typography variant="body2" color="textDisabled">
+                    Osmania University, Hyderabad
+                  </Typography>
+                </Stack>
+              </Stack>
+              <Stack direction={"row"} spacing={2} alignItems={"center"}>
+                <SchoolIcon />
+                <Stack>
+                  <Typography
+                    variant="body1"
+                    color="textDisabled"
+                    fontWeight={500}
+                  >
+                    Master in Computer Application
+                  </Typography>
+                  <Typography variant="body2" color="textDisabled">
+                    Savitribai Phule Pune University, Pune
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Stack>
+          </Stack>
+          <br />
+          <hr style={{ opacity: 0.4 }} />
+          <Stack
+            spacing={2}
+            direction={"row"}
+            justifyContent={"space-between"}
+            marginTop={5}
+            alignItems={"center"}
+          >
+            <Stack
+              border="1px solid grey"
+              borderRadius={3}
+              padding={2}
+              width={250}
             >
-              Download Resume
-            </Typography>
-          </Button>
-
-          <Grid container direction={"row"} justifyContent={"space-between"}>
-            <Grid container item size={6}>
-              <Grid item size={12}>
-                <Typography variant="h6" fontWeight={600}>
-                  Interests
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Stack spacing={1}>
-                  <Typography>• Web Development</Typography>
-                  {/* <Typography>• Artificial Intelligence</Typography> */}
-                  <Typography>• Cloud Computing</Typography>
-                  <Typography>• Blockchain and Defi</Typography>
-                  <Typography>• DevOps and CloudOps</Typography>
-                </Stack>
-              </Grid>
-            </Grid>
-            <Grid container item size={6} spacing={1}>
-              <Grid item size={12}>
-                <Typography variant="h6" fontWeight={600}>
-                  Education
-                </Typography>
-              </Grid>
-              <Grid item size={12}>
-                <Stack direction={"row"} spacing={2} alignItems={"center"}>
-                  <SchoolIcon />
-                  <Stack>
-                    <Typography>HSC</Typography>
-                    <Typography>Gen. BC Joshi Army Public School</Typography>
-                  </Stack>
-                </Stack>
-              </Grid>
-              <Grid item size={12}>
-                <Stack direction={"row"} spacing={2} alignItems={"center"}>
-                  <SchoolIcon />
-                  <Stack>
-                    <Typography>BCA</Typography>
-                    <Typography>Osmania University</Typography>
-                  </Stack>
-                </Stack>
-              </Grid>
-              <Grid item size={12}>
-                <Stack direction={"row"} spacing={2} alignItems={"center"}>
-                  <SchoolIcon />
-                  <Stack>
-                    <Typography>MCA Cloud Engineering</Typography>
-                    <Typography>Savitribai Phule Pune University</Typography>
-                  </Stack>
-                </Stack>
-              </Grid>
-            </Grid>
-          </Grid>
+              <Typography variant="subtitle1" fontWeight={700}>
+                {" "}
+                20+{" "}
+              </Typography>
+              <Typography variant="body2" color="textDisabled">
+                Projects Completed{" "}
+              </Typography>
+            </Stack>
+            <Stack
+              border="1px solid grey"
+              borderRadius={3}
+              padding={2}
+              width={250}
+            >
+              <Typography variant="subtitle1" fontWeight={700}>
+                {" "}
+                5+{" "}
+              </Typography>
+              <Typography variant="body2" color="textDisabled">
+                {" "}
+                Years of Coding Experience{" "}
+              </Typography>
+            </Stack>
+            <Stack
+              border="1px solid grey"
+              borderRadius={3}
+              padding={2}
+              width={250}
+            >
+              <Typography variant="subtitle1" fontWeight={700}>
+                {" "}
+                3{" "}
+              </Typography>
+              <Typography variant="body2" color="textDisabled">
+                {" "}
+                Years of Industry Experience{" "}
+              </Typography>
+            </Stack>
+          </Stack>
         </Stack>
       </div>
     </div>
