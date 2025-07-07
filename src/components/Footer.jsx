aed6deac4e592153707d13243eb003173e6d66bd
venom96 <rajatmakholiya07@gmail.com>
2025-07-07 13:09:20 +0530

    code cleaning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Typography, Stack, IconButton } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,14 +6,10 @@ import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
 import TimelineDot from "@mui/lab/TimelineDot";
-import FastfoodIcon from "@mui/icons-material/Fastfood";
 import LaptopMacIcon from "@mui/icons-material/LaptopMac";
-import HotelIcon from "@mui/icons-material/Hotel";
-import RepeatIcon from "@mui/icons-material/Repeat";
 import SmartToyIcon from "@mui/icons-material/SmartToy";
 import Typography from "@mui/material/Typography";
 import CodeIcon from "@mui/icons-material/Code";
-import { Button } from "antd";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const Experience = () => {
