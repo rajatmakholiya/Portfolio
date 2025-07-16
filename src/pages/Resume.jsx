5aec4e45d2634133ef1dba175cc884072ce4c45d
rajatmakholiya <rajatmakholiya07@gmail.com>
2025-07-16 20:01:42 +0530

    resume (mobile)

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,12 @@
-import { Cloudy,FileCode, Brain } from "lucide-react";
+import { Cloudy, FileCode, Brain } from "lucide-react";
 import SmartButton from "../components/sub-components/SmartButton";
-const Resume = ({currentTheme}) => {
+const Resume = ({ currentTheme }) => {
   return (
     <div className="h-full flex items-center justify-center text-center mt-48">
-      <div className="flex justify-between w-full max-w-4xl px-8 ">
-        <SmartButton content={"DevOps and Cloud Operations"} Icon={Cloudy} theme={currentTheme} source={"/resume/devops"}/>
-        <SmartButton content={"Web Developer"} Icon={FileCode} theme={currentTheme} source={"/resume/wd"}/>
-        <SmartButton content={"AI/ML"} Icon={Brain} theme={currentTheme} source={"/resume/aiml"}/>
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-between w-full max-w-4xl px-8 gap-8 md:gap-4">
+        <SmartButton content={"DevOps and Cloud Operations"} Icon={Cloudy} theme={currentTheme} source={"/resume/devops"} />
+        <SmartButton content={"Web Developer"} Icon={FileCode} theme={currentTheme} source={"/resume/wd"} />
+        <SmartButton content={"AI/ML"} Icon={Brain} theme={currentTheme} source={"/resume/aiml"} />
       </div>
     </div>
   );
