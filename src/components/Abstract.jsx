b2c29013b793cf1c906ddeab627c3932c5ac5cae
venom96 <rajatmakholiya07@gmail.com>
2025-07-17 21:22:15 +0530

    ui changes

--- src/components/Abstract.jsx
+++ src/components/Abstract.jsx
@@ -3,7 +3,7 @@ import AbstractBio from "./AbstractBio";
 
 const Abstract = () => {
   return (
-    <div className="relative w-full md:h-[83vh] min-h-[500px] bg-[url('https://images.pexels.com/photos/192136/pexels-photo-192136.jpeg')] rounded-xl bg-cover bg-center flex items-center justify-center p-4">
+    <div className="relative w-[85vw] md:h-[83vh] min-h-[500px] bg-[url('https://images.pexels.com/photos/192136/pexels-photo-192136.jpeg')] rounded-xl bg-cover bg-center flex items-center justify-center p-4 ">
       <div className="absolute inset-0 backdrop-blur-md bg-black/10 rounded-xl"></div>
       <div className="relative w-full">
         <AbstractBio />
